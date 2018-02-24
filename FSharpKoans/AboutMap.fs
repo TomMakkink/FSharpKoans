b8efe41f7deee86d2557096ba5a2f5025c5f97a5
desluk <lukedestombe.ldt@gmail.com>
2018-02-24 22:50:15 +0200

    Did the first 2 tests

--- FSharpKoans/AboutMap.fs
+++ FSharpKoans/AboutMap.fs
@@ -9,7 +9,13 @@ module ``15: Applying a map to a list`` =
     [<Test>]
     let ``01 Fixed-function mapping, the hard way (part 1).`` () =
         let map (xs : int list) : int list =
-            __ // write a function which adds 1 to each element
+          let rec add inList outList =
+           match inList with 
+           | [] ->List.rev outList
+           | n::rest -> add rest (n+1::outList)
+          add xs [] 
+            
+            // write a function which adds 1 to each element
         map [1; 2; 3; 4] |> should equal [2; 3; 4; 5]
         map [9; 8; 7; 6] |> should equal [10; 9; 8; 7]
         map [15; 2; 7] |> should equal [16; 3; 8]
@@ -19,7 +25,11 @@ module ``15: Applying a map to a list`` =
     [<Test>]
     let ``02 Fixed-function mapping, the hard way (part 2).`` () =
         let map (xs : int list) : int list =
-            __ // write a function which doubles each element
+          let rec add inList outList =
+           match inList with 
+           | [] ->List.rev outList
+           | n::rest -> add rest (n*2::outList)
+          add xs [] // write a function which doubles each element
         map [1; 2; 3; 4] |> should equal [2; 4; 6; 8]
         map [9; 8; 7; 6] |> should equal [18; 16; 14; 12]
         map [15; 2; 7] |> should equal [30; 4; 14]
@@ -38,7 +48,7 @@ module ``15: Applying a map to a list`` =
     [<Test>]
     let ``03 Specified-function mapping, the hard way`` () =
         let map (f : 'a -> 'b) (xs : 'a list) : 'b list =
-            __ // write a map which applies f to each element
+            __  // write a map which applies f to each element
         map (fun x -> x+1) [9;8;7] |> should equal [10;9;8]
         map ((*) 2) [9;8;7] |> should equal [18;16;14]
         map (fun x -> sprintf "%.2f wut?" x)  [9.3; 1.22] |> should equal ["9.30 wut?"; "1.22 wut?"]
